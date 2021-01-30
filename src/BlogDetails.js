bafefef224b114bd911f393d2378e1159b37e81b
saikat1095 <mdabutahersaikat@gmail.com>
2021-01-30 09:48:47 +0600

    blogdetails.js is included

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useFetch from './useFetch';
 
 export const BlogDetails = () => {
 
     const { id } = useParams();
+    const {data, error, isPending} = useFetch('http://localhost:8000/blogs/' + id);
     return (
         <div className="blog-details">
             <h2>Blog details - { id }</h2>
